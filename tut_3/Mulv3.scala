aa18d6709fe7d750245e3b752b71fd276a86f5ca
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-08-19 10:10:17 +1000

    Workig multiplier with v3 algorithm

--- tut_3/Mulv3.scala
+++ tut_3/Mulv3.scala
@@ -10,24 +10,22 @@ class Mulv3dpath(val n: Int) extends Module {
     val y = UInt(INPUT, n)
     val z = UInt(OUTPUT, 2*n)
     val load = Bool(INPUT)
-    val lshift = Bool(INPUT)
     val rshift = Bool(INPUT)
     val write = Bool(INPUT)
   }
-  val multiplier = Reg(init = UInt(0, width = n));
-  val multiplicand = Reg(init = UInt(0, width = 2 * n));
-  val product = Reg(init = UInt(0, width = 2 * n));
+  val multiplicand = Reg(init = UInt(0, width = n));
+  val product = Reg(init = UInt(0, width = (2*n)+1));
 
   when (io.load) {
     multiplicand := io.x
-    multiplier := io.y
-    product := UInt(0)
+    product := Cat(UInt(0, width=n+1), io.y) 
   }
-  when (io.lshift) { multiplicand := multiplicand << UInt(1) }
-  when (io.rshift) { multiplier := multiplier >> UInt(1) }
-  when (io.write && (multiplier(0) === UInt(1))) { 
-    product := product + multiplicand 
+  when (io.write && (product(0) === UInt(1))) { 
+    val left_side = product(2*n, n) + multiplicand
+    val right_side = product(n-1, 0)
+    product := Cat(left_side, right_side) >> UInt(1)
   } 
+  when (io.rshift) { product := product >> UInt(1) }
   io.z := product
 }
 
@@ -46,17 +44,18 @@ class Mulv3(val n: Int) extends Module {
   m.io.x := io.x
   m.io.y := io.y
 
-  m.io.load := Bool(false)
+  m.io.load := io.load
   m.io.rshift := Bool(false)
-  m.io.lshift := Bool(false)
   m.io.write := Bool(false)
 
-  when (state === UInt(0)) { 
+  when (io.load) {
+    state := UInt(0)
     m.io.load := io.load
-  } .elsewhen (state(0) === UInt(0)) { 
+  }.
+  elsewhen (m.io.z(0) === UInt(0)) { 
     m.io.rshift := Bool(true)
-    m.io.lshift := Bool(true)
-  } .elsewhen (state(0) === UInt(1)) { 
+  }.
+  elsewhen (m.io.z(0) === UInt(1)) { 
     m.io.write := Bool(true)
   }
   state := state + UInt(1)
@@ -71,6 +70,7 @@ class Mulv3Tester(c: Mulv3, n: Int) extends Tester(c) {
     poke(c.io.y, y)
     poke(c.io.load, 1)
     step(1)
+    expect(c.m.product, y)
     poke(c.io.load, 0)
     step(n)
     expect(c.io.z, (x * y))
