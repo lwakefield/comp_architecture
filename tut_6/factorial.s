be631c2a6ec14e6ebd8c08bf793672c5a7d48c0c
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-09-16 19:40:03 +1000

    Factorial is working

--- tut_6/factorial.s
+++ tut_6/factorial.s
@@ -1,17 +1,37 @@
+        .data
+str:
+        .asciiz "Fact is: "
+newline:
+        .asciiz "\n"
+
+        .text
 main:
-        li      $a0, 5
+        li      $t1, 0
+        li      $v1, 9
+L2:
+        add     $a0, $t1, $zero
         jal     fact
-        ;li      $v0, 1
-        ;syscall
-        jr      $ra
+        add     $a1, $v0, $zero
+        li      $v0, 4
+        la      $a0, str
+        syscall
+        add     $a0, $a1, $zero
+        li      $v0, 1
+        syscall
+        li      $v0, 4
+        la      $a0, newline
+        syscall
+        addi    $t1, $t1, 1
+        ble     $t1, $v1, L2
+        li      $v0, 10
+        syscall
 fact:
         addi $sp, $sp, -8     # adjust stack for 2 items
         sw   $ra, 4($sp)      # save return address
         sw   $a0, 0($sp)      # save argument
-        li   $v0, 1
         slti $t0, $a0, 1      # test for n < 1
         beq  $t0, $0, L1
-        addi $v0, $0, 1       # if so, result is 1
+        addi $v0, $0, 1    # if so, result is 1
         addi $sp, $sp, 8      #   pop 2 items from stack
         jr   $ra              #   and return
 L1:     addi $a0, $a0, -1     # else decrement n
